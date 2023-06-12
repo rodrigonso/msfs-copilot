9a78368481c5fd77c55eb581cf8087ea37d8f069
Rodrigo Mascarenhas <rodrigocm08@gmail.com>
2023-06-11 20:46:42 -0700

    refactor to keep code clean

--- main.py
+++ main.py
@@ -1,57 +1,10 @@
 import re
 import nltk
-from PyPDF2 import PdfReader
 import pdfplumber
 import math
+import json
 
-def read_PDF(file):
-    reader = PdfReader(file)
-    num_of_pages = len(reader.pages)
-
-    page = reader.pages[0]
-    text = page.extract_text()
-
-    # print(text)
-    return text
-
-
-# There's an issue here where some of the checklist sections don't get properly split because they are in the same line as the item. may require manual fixing of this here (review)
-def parse_PDF(text):
-    result = []
-
-    bullet_point = re.compile(r"\u2022")
-    last_title_idx = 0
-
-    lines = text.split('\n')
-    curr = []
-
-    regex = "^[A-Z ]*$"
-    for idx, line in enumerate(lines):
-        if (re.search(bullet_point, line)):
-            res = line.strip()
-            cleanString = re.sub(bullet_point, '', res)
-
-            test = cleanString
-
-            curr.append(test)   
-        else:
-            regex = "^[A-Z ]*$"
-            test = bool(re.search(regex, line))
-            if is_valid_token(line) and test:
-                result.append({"name": lines[last_title_idx].strip(), "values": curr})
-                last_title_idx = idx
-                curr = []
-
-    return result
-
-def is_valid_token(token):
-    return token and token.strip()
-
-def is_upper(string):
-    return string.isupper() and string.isalpha()
-
-
-def rearrenge_tokens(values, section_name):
+def prepare_to_JSONL(values, section_name):
     res = []
 
     for idx, item in enumerate(values):
@@ -94,49 +47,67 @@ def extract_key_values(text):
 
     return {'q': q, 'a': a}
 
+def format_data_from_text(extracted_text, sections):
+    section = {'name': '', 'values': []}
+    prev_title = ''
+    curr_title = ''
+    for line in extracted_text:
+        title = extract_title(line)
+        if title:
+            curr_title = title
+            section = {'name': curr_title, 'values': []}
+        else:
+            if prev_title != curr_title:
+                sections.append(section)
+                prev_title = curr_title
+
+            key_val = extract_key_values(line)
+            section['values'].append(key_val)
+
+    return section
+
 def main():
     files = ["./test.pdf", "./test2.pdf", "./test3.pdf", "./test4.pdf", "./test5.pdf"]
     pdf_file = pdfplumber.open('./test2.pdf')
 
-
     sections = []
     for p, char in zip(pdf_file.pages, pdf_file.chars):
-        # texts = p.extract_tables({'vertical_strategy': 'text', 'horizontal_strategy': 'text', "text_tolerance": 10})
+
         width = p.width
         height = p.height
 
         first_half_x = width / 2
-        first_half_y = height / 2
+        second_half_x = width - first_half_x
 
-                        #x0, top, x1, bottom
+        # to make it easier to extract the data, we split the pdf page into two halves along the y axis and extract page by page
         cropped = p.crop((0, 0, first_half_x, height))
+        cropped2 = p.crop((second_half_x, 0, width, height))
 
-
-        # skip first page since does not contain actual ch
+        # skip first page since does not contain actual useful data (apart from aircraft info which might be useful later)
         if p.page_number < 2:
             continue
 
+        # extract left half of page
         extracted = cropped.extract_text_lines()
 
-        section = {'name': '', 'values': []}
-        prev_title = ''
-        curr_title = ''
-        for line in extracted:
-            
-            title = extract_title(line)
-            if title:
-                curr_title = title
-                section = {'name': curr_title, 'values': []}
-            else:
-                if prev_title != curr_title:
-                    sections.append(section)
-                    prev_title = curr_title
+        #extract right half of page
+        extracted2 = cropped2.extract_text_lines()
 
-                key_val = extract_key_values(line)
-                section['values'].append(key_val)
+        format_data_from_text(extracted, sections)
+        format_data_from_text(extracted2, sections)
 
-        # sections.append(section)
-    
+    pdf_file.close()
+
+    data_for_training = []
+    for section in sections:
+        data_for_training.append(prepare_to_JSONL(section['values'], section['name']))
+
+    with open('training.json', "w") as text_file:
+        for item in data_for_training:
+            for line in item:
+                text_file.write(json.dumps(line))
+                text_file.write("\n")
+        
 
 if __name__ == "__main__":
     main()
